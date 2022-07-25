3b14d035f2e7716a2d27c8367c5d1d8c5479ae80
alankruthi-21 <2215316621@gitam.in>
2022-07-25 19:21:50 +0530

    build1

---
+++ jenkinsfile_build.groovy
@@ -0,0 +1,28 @@
+pipeline{
+    agent any
+    parameters{
+        string(name: 'BRANCH', defaultValue: '', description: '')
+    }
+    stages{
+        stage("clone a code"){
+            steps{
+                println "clone a code"
+                git branch:'${BRANCH}',
+                url:'https://github.com/KuruvaSomaSekhar/boxfuse-sample-java-war-hello.git'
+            }
+        }
+        stage("build"){
+            steps{
+                println "build a code"
+                sh "mvn clean package",
+                sh "ls -l"
+            }
+        }
+        stage("uploading the artifacts"){
+            steps{
+                println "uploading the artifacts to s3"
+                sh "aws s3 cp target/hello-*.war s3://alankruthiart/${BRANCH}/"
+            }
+        }
+    }
+}
